de106ca0cf6b8249975977491e79fb0c85611543
Sergey Zolotarev <sryze@protonmail.com>
2018-05-27 19:29:29 +0600

    Add commands to encode/decode full URI

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # URI Encode/Decode
 
-This is a simple extension that URI-encodes or decodes a piece of text. You simply select a piece of URL and use Command Palette -> Decode (or Encode) Selection as URI Component.
+This is a simple extension that URI-encodes or decodes a piece of text as a full URI as as 
+URI component.
+
+## Features
+
+* Select a URL and open command palette -> `Encode as URI` or `Decode as URI` to encode/decode as
+  a full URL. This ensures that only the necessary parts are encode, i.e. URL parameters.
+
+* Select a part of a URL and open command palette -> `Encode as URI Component` or 
+  `Decode as URI Component` to encode/decode as a URL parameter. Usually you only want to use this
+  if you select the value of some parameter after the `=` sign and you want to encode/decode only
+  that piece.
 
 ## Release Notes
 
--- src/extension.ts
+++ src/extension.ts
@@ -3,33 +3,67 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.encodeURIComponent', () => {
-        let editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return; // No open text editor
-        }
+    const encodeURICommand = 
+        vscode.commands.registerCommand('extension.encodeURI', () => {
+            const editor = vscode.window.activeTextEditor;
+            if (!editor) {
+                return; // No open text editor
+            }
 
-        let selection = editor.selection;
-        let text = editor.document.getText(selection);
+            const selection = editor.selection;
+            const text = editor.document.getText(selection);
 
-        editor.edit(editBuilder => {
-            editBuilder.replace(selection, encodeURIComponent(text));
+            editor.edit(editBuilder => {
+                editBuilder.replace(selection, encodeURI(text));
+            });
         });
-    });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(encodeURICommand);
 
-    disposable = vscode.commands.registerCommand('extension.decodeURIComponent', () => {
-        let editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return; // No open text editor
-        }
+    const encodeURIComponentCommand = 
+        vscode.commands.registerCommand('extension.encodeURIComponent', () => {
+            const editor = vscode.window.activeTextEditor;
+            if (!editor) {
+                return; // No open text editor
+            }
 
-        let selection = editor.selection;
-        let text = editor.document.getText(selection);
+            const selection = editor.selection;
+            const text = editor.document.getText(selection);
 
-        editor.edit(editBuilder => {
-            editBuilder.replace(selection, decodeURIComponent(text));
+            editor.edit(editBuilder => {
+                editBuilder.replace(selection, encodeURIComponent(text));
+            });
         });
-    });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(encodeURIComponentCommand);
+
+    const decodeURICommand = 
+        vscode.commands.registerCommand('extension.decodeURI', () => {
+            const editor = vscode.window.activeTextEditor;
+            if (!editor) {
+                return; // No open text editor
+            }
+
+            const selection = editor.selection;
+            const text = editor.document.getText(selection);
+
+            editor.edit(editBuilder => {
+                editBuilder.replace(selection, decodeURI(text));
+            });
+        });
+    context.subscriptions.push(decodeURICommand);
+
+    const decodeURIComponentCommand = 
+        vscode.commands.registerCommand('extension.decodeURIComponent', () => {
+            const editor = vscode.window.activeTextEditor;
+            if (!editor) {
+                return; // No open text editor
+            }
+
+            const selection = editor.selection;
+            const text = editor.document.getText(selection);
+
+            editor.edit(editBuilder => {
+                editBuilder.replace(selection, decodeURIComponent(text));
+            });
+        });
+    context.subscriptions.push(decodeURIComponentCommand);
 }
