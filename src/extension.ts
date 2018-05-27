82f096f96ddaece54733ea77e5a40fb49f4b43ed
Sergey Zolotarev <sryze@protonmail.com>
2018-05-27 20:45:34 +0600

    Refactor code to reduce duplication

--- src/extension.ts
+++ src/extension.ts
@@ -2,68 +2,39 @@
 
 import * as vscode from 'vscode';
 
+function transformEditorSelection(transformer: (text: string) => string) {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return; // No open text editor
+    }
+
+    const selection = editor.selection;
+    const text = editor.document.getText(selection);
+
+    editor.edit(editBuilder => {
+        editBuilder.replace(selection, transformer(text));
+    });
+}
+
 export function activate(context: vscode.ExtensionContext) {
-    const encodeURICommand = 
+    context.subscriptions.push(
         vscode.commands.registerCommand('extension.encodeURI', () => {
-            const editor = vscode.window.activeTextEditor;
-            if (!editor) {
-                return; // No open text editor
-            }
-
-            const selection = editor.selection;
-            const text = editor.document.getText(selection);
-
-            editor.edit(editBuilder => {
-                editBuilder.replace(selection, encodeURI(text));
-            });
-        });
-    context.subscriptions.push(encodeURICommand);
-
-    const encodeURIComponentCommand = 
+            transformEditorSelection(encodeURI);
+        })
+    );
+    context.subscriptions.push(
         vscode.commands.registerCommand('extension.encodeURIComponent', () => {
-            const editor = vscode.window.activeTextEditor;
-            if (!editor) {
-                return; // No open text editor
-            }
-
-            const selection = editor.selection;
-            const text = editor.document.getText(selection);
-
-            editor.edit(editBuilder => {
-                editBuilder.replace(selection, encodeURIComponent(text));
-            });
-        });
-    context.subscriptions.push(encodeURIComponentCommand);
-
-    const decodeURICommand = 
+            transformEditorSelection(encodeURIComponent);
+        })
+    );
+    context.subscriptions.push(
         vscode.commands.registerCommand('extension.decodeURI', () => {
-            const editor = vscode.window.activeTextEditor;
-            if (!editor) {
-                return; // No open text editor
-            }
-
-            const selection = editor.selection;
-            const text = editor.document.getText(selection);
-
-            editor.edit(editBuilder => {
-                editBuilder.replace(selection, decodeURI(text));
-            });
-        });
-    context.subscriptions.push(decodeURICommand);
-
-    const decodeURIComponentCommand = 
+            transformEditorSelection(decodeURI);
+        })
+    );
+    context.subscriptions.push(
         vscode.commands.registerCommand('extension.decodeURIComponent', () => {
-            const editor = vscode.window.activeTextEditor;
-            if (!editor) {
-                return; // No open text editor
-            }
-
-            const selection = editor.selection;
-            const text = editor.document.getText(selection);
-
-            editor.edit(editBuilder => {
-                editBuilder.replace(selection, decodeURIComponent(text));
-            });
-        });
-    context.subscriptions.push(decodeURIComponentCommand);
+            transformEditorSelection(decodeURIComponent);
+        })
+    );
 }
